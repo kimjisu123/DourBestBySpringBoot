8a58c2cea55b895f90f4df2df52f415b46fe2253
j2seon <ddj04323@gmail.com>
2023-06-21 18:17:07 +0900

    first commit

---
+++ src/main/java/com/won/dourbest/DourbestApplication.java
@@ -0,0 +1,13 @@
+package com.won.dourbest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DourbestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DourbestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/won/dourbest/DourbestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.won.dourbest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DourbestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
