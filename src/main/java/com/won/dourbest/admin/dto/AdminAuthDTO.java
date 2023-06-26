e8a5fb0314d4df8930d3c7d93edb4bc5ccd1e751
JISU <dkljjh10@gmail.com>
2023-06-26 13:14:19 +0900

    commit

---
+++ src/main/java/com/won/dourbest/admin/dto/AdminAuthDTO.java
@@ -0,0 +1,17 @@
+package com.won.dourbest.admin.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdminAuthDTO {
+
+
+    private String adminAuthName;
+    private String authName;
+    private String authDesc;
+
+}
