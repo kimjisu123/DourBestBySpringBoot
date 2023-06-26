e0ff4670908e0491d8b785070ef54897738795e0
DESKTOP-TFLQRBN\jihopark <wlghsms987@gmail.com>
2023-06-26 13:16:26 +0900

    jiho commit

---
+++ src/main/java/com/won/dourbest/seller/dto/TagDTO.java
@@ -0,0 +1,14 @@
+package com.won.dourbest.seller.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class TagDTO {
+
+    private int tagCode;
+    private String tagName;
+}
