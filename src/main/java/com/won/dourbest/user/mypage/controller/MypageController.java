ffbcc5ba9851a3c6a550a0745773211e2f25b449
j2seon <ddj04323@gmail.com>
2023-06-27 09:10:42 +0900

    mapper 추가

--- src/main/java/com/won/dourbest/user/mypage/controller/MypageController.java
+++ src/main/java/com/won/dourbest/user/mypage/controller/MypageController.java
@@ -1,4 +1,4 @@
-package com.won.dourbest.user.mypage;
+package com.won.dourbest.user.mypage.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
---
+++ src/main/java/com/won/dourbest/user/mypage/repository/MypageMapper.java
@@ -0,0 +1,7 @@
+package com.won.dourbest.user.mypage.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MypageMapper {
+}
